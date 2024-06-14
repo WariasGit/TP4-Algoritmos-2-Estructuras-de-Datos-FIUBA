44e6a4489b037e178ae4465526083d5149d86b19
FedeWSL <warias@fi.uba.ar>
2024-06-14 02:48:02 -0300

    Creo la clase utiles, con metodos necesarios para el juego. Ademas modifico el Cmakelist y creo un arhivo vacio para las constantes

---
+++ include/utiles.hpp
@@ -0,0 +1,51 @@
+#ifndef AYED_TPG_1C2024_UTILES_HPP
+#define AYED_TPG_1C2024_UTILES_HPP
+
+#include <iostream>
+#include "coordenada.hpp"
+#include <random>
+
+enum UTILES{
+    INICIA_EN_CERO = 0,
+};
+
+class utiles{
+public:
+    /*
+    * Pre:
+    * Post: Devuelve una opcion valida.
+    */
+    static int validar_ingreso_opcion(std::string input, int minimo, int maximo);
+
+    /*
+    * Pre:
+    * Post: Devuelve true si el input es numerico.
+    */
+    static bool es_numerico(std::string input);
+
+    /*
+    * Pre:
+    * Post: Devuelve true si el input corresponde a una opcion valida.
+    */
+    static bool es_numero_valido(std::string input, int opcion_min, int opcion_max);
+
+    /*
+    * Pre: Minimo y maximo deben ser valores validos. Minimo debe ser menor o igual que maximo.
+    * Post: Devuelve un numero entero aleatorio en el rango [minimo, maximo]. 
+    *       El numero aleatorio generado, sigue una distribucion uniforme dentro del rango.
+    *       Cada llamada produce un resultado independiente del resto de llamadas.
+    */
+    static size_t generar_numero_aleatorio(size_t minimo, size_t maximo);
+
+    /*
+    * Pre: Maximo debe ser un valor valido y mayor a cero.
+    * Post: Devuelve un numero entero aleatorio en el rango [cero, maximo]. 
+    *       El numero aleatorio generado, sigue una distribucion uniforme dentro del rango.
+    *       Cada llamada produce un resultado independiente del resto de llamadas.
+    */
+    static size_t generar_numero_aleatorio(size_t maximo);
+
+};
+
+
+#endif
---
+++ src/utiles.cpp
@@ -0,0 +1,59 @@
+#include "utiles.hpp"
+
+int utiles::validar_ingreso_opcion(std::string input, int minimo, int maximo){
+    while (!(es_numero_valido(input, minimo, maximo)))
+    {
+        if(!es_numerico(input))
+        {
+            std::cout << "Ingrese un valor numerico valido." << std::endl;
+        } 
+        else
+        {
+            std::cout << "Fuera de rango, ingrese una opcion valida." << std::endl;
+        }
+    }
+    std::cout << "Ingreso la opcion: " << input << std::endl;
+    return (stoi(input));
+    
+}
+
+bool utiles::es_numerico(std::string input){
+    int i = 0;
+    bool es_numero = true;
+    while (es_numero && i < int(input.length()) ) {
+        if ( !(isdigit(input[i])) ) {
+            es_numero = false;
+        }
+        i++;
+    }
+    return es_numero;
+}
+
+bool utiles::es_numero_valido(std::string input, int opcion_min, int opcion_max){
+    bool es_valido = true;
+    if (!es_numerico(input))
+    {
+        es_valido = false;
+    }
+    else if (stoi(input)<=opcion_min || stoi(input)>=opcion_max)
+    {
+        es_valido = false;
+    }
+    return es_valido;
+    
+}
+
+size_t utiles::generar_numero_aleatorio(size_t minimo, size_t maximo){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<size_t> dis(minimo, maximo);
+    return dis(gen);
+}
+
+size_t utiles::generar_numero_aleatorio(size_t maximo){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<size_t> dis(INICIA_EN_CERO, maximo);
+    return dis(gen);
+}
+
