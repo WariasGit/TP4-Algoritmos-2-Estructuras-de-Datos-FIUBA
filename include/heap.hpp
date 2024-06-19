1c0e089720b9ddc05931dab6d6914e31aa06e886
Bserrantes <bserrantes@.fi.uba.ar>
2024-06-19 19:36:27 -0300

    Refactorizacion del heap e implementación del contador de up/downheaps

--- include/heap.hpp
+++ include/heap.hpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <cstdlib>
 #include <exception>
-#include <random>
 
 class heap_exception : public std::exception {
 };
@@ -13,10 +12,6 @@ template<typename T, bool comp(T, T)>
 class heap {
 private:
     std::vector<T> datos;
-
-    //Pre: Necesita de dos cotas, una inferior y una superior.
-    //Post: Devuelve un numero entero que va a estar entre las dos cotas dadas.
-    int rand_int_gen(int cota_inf, int cota_sup);
 
     // Pre: Ambos índices deben ser menor que la cantidad de datos.
     // Post: Intercambia de lugar los datos de los indices indicados.
@@ -37,6 +32,14 @@ private:
     // Consideren cada caso con detenimiento.
     // Adicionalmente, tengan cuidado con el casteo de las variables, porque son size_t.
     // Hacer, por ejemplo, size_t i = 0; i - 1; produce un underflow.
+
+    //Pre: El indice debe ser menor que la cantidad de datos.
+    //Post: Realiza la cantidad de upheaps necesarios para acomodar el heap y devuelve esa cantidad.
+    size_t contar_upheaps(size_t& indice);
+
+    //Pre: El indice debe ser menor que la cantidad de datos.
+    //Post: Realiza la cantidad de downheaps necesarios para acomodar el heap y devuelve esa cantidad.
+    size_t contar_downheaps(size_t& indice);
 public:
     // Constructor.
     heap();
@@ -69,25 +72,55 @@ public:
 
     // Destructor.
     ~heap();
+
 };
 
-template<typename T, bool (* comp)(T, T)>
-heap<T, comp>::heap() {
-
-}
-
-template<typename T, bool (*comp)(T,T)>
-int heap<T, comp>::rand_int_gen(int cota_inf, int cota_sup){
-    int rand_num;
-    random_device rd;
-    uniform_int_distribution<int> dist(cota_inf,cota_sup);
-    rand_num = dist(rd);
-    return rand_num;
+template <typename T, bool (*comp)(T, T)>
+size_t heap<T, comp>::contar_upheaps(size_t& indice){
+    size_t upheaps = 0;
+    size_t pos_padre = (indice-1)/2;
+    while (indice > 0 && datos[pos_padre] < datos[indice]){
+        swap(pos_padre, indice);
+        upheaps++;
+        indice = pos_padre;
+        pos_padre = (indice-1)/2;
+    }
+    return upheaps;
 }
 
 template <typename T, bool (*comp)(T, T)>
-void heap<T, comp>::swap(size_t indice_1, size_t indice_2)
-{
+size_t heap<T, comp>::contar_downheaps(size_t &indice){
+    size_t downheaps = 0;
+    size_t pos_hijo_izq = (indice*2)+1;
+    size_t pos_hijo_der = (indice*2)+2;
+    size_t pos_mayor = indice;
+    while((pos_hijo_der < datos.size() && datos[pos_hijo_der] > datos[pos_mayor]) || (pos_hijo_izq < datos.size() && datos[pos_hijo_izq] > datos[pos_mayor])){
+
+        if (pos_hijo_der < datos.size() && datos[pos_hijo_der] > datos[pos_mayor]){
+            pos_mayor = pos_hijo_der;
+        }
+        if (pos_hijo_izq < datos.size() && datos[pos_hijo_izq] > datos[pos_mayor]){
+            pos_mayor = pos_hijo_izq;
+        }
+
+        if(pos_mayor != indice){
+            swap(pos_mayor, indice);
+            downheaps++;
+            indice = pos_mayor;
+            pos_hijo_izq = (indice*2)+1;
+            pos_hijo_der = (indice*2)+2;
+        }
+    }
+    return downheaps;
+}
+
+template <typename T, bool (*comp)(T, T)>
+heap<T, comp>::heap(){
+
+}
+
+template <typename T, bool (*comp)(T, T)>
+void heap<T, comp>::swap(size_t indice_1, size_t indice_2){
     if(indice_1 < datos.size() && indice_2 < datos.size()){
         T aux = datos[indice_2];
         datos[indice_2] = datos[indice_1];
@@ -97,31 +130,36 @@ void heap<T, comp>::swap(size_t indice_1, size_t indice_2)
 
 template<typename T, bool (* comp)(T, T)>
 void heap<T, comp>::upheap(size_t& indice) {
-    while (indice > 0 && indice < datos.size()){
-        size_t pos_padre = (indice-1)/2;
-        if(datos[pos_padre] < datos[indice]){
-            swap(pos_padre, indice);
-            indice = pos_padre;
-        }
+
+    size_t pos_padre = (indice-1)/2;
+    while (indice > 0 && datos[pos_padre] < datos[indice]){
+        swap(pos_padre, indice);
+        upheaps++;
+        indice = pos_padre;
+        pos_padre = (indice-1)/2;
     }
 }
 
 template<typename T, bool (* comp)(T, T)>
 void heap<T, comp>::downheap(size_t& indice) {
-    while(indice < datos.size() && indice > 0){
-        size_t pos_hijo_izq = (indice*2)+1;
-        size_t pos_hijo_der = (indice*2)+2;
-        size_t pos_mayor;
-        if (datos[pos_hijo_der] > datos[pos_hijo_izq]){
+
+    size_t pos_hijo_izq = (indice*2)+1;
+    size_t pos_hijo_der = (indice*2)+2;
+    size_t pos_mayor = indice;
+    while((pos_hijo_der < datos.size() && datos[pos_hijo_der] > datos[pos_mayor]) || (pos_hijo_izq < datos.size() && datos[pos_hijo_izq] > datos[pos_mayor])){
+
+        if (pos_hijo_der < datos.size() && datos[pos_hijo_der] > datos[pos_mayor]){
             pos_mayor = pos_hijo_der;
         }
-        else{
+        if (pos_hijo_izq < datos.size() && datos[pos_hijo_izq] > datos[pos_mayor]){
             pos_mayor = pos_hijo_izq;
         }
 
-        if(pos_mayor < datos.size() && datos[pos_mayor]){
+        if(pos_mayor != indice){
             swap(pos_mayor, indice);
             indice = pos_mayor;
+            pos_hijo_izq = (indice*2)+1;
+            pos_hijo_der = (indice*2)+2;
         }
     }
 }
@@ -154,19 +192,38 @@ T heap<T, comp>::baja() {
 
 template<typename T, bool (* comp)(T, T)>
 size_t heap<T, comp>::alta_complejidad(T dato) {
-    return size_t;
+    size_t cant_upheaps = 0;
+    if(vacio()){
+        datos.push_back(dato);
+    }
+    else{
+        datos.push_back(dato);
+        size_t pos_dato_nuevo = datos.size() - 1;
+        cant_upheaps = contar_upheaps(pos_dato_nuevo);
+    }
+    return cant_upheaps;
 }
 
 template<typename T, bool (* comp)(T, T)>
 size_t heap<T, comp>::baja_complejidad() {
-    return size_t;
+    size_t cant_downheaps = 0;
+    if(!vacio()){
+        size_t pos_eliminado = 0;
+        size_t pos_ultimo = datos.size() - 1;
+        swap(pos_eliminado,pos_ultimo);
+        cant_downheaps = contar_downheaps(pos_eliminado);
+        datos.pop_back();
+    }
+    return cant_downheaps;
 }
 
 template<typename T, bool (* comp)(T, T)>
 T heap<T, comp>::primero() {
+    T primer_dato;
     if (!vacio()){
-        return datos.front(); 
+        primer_dato = datos.front(); 
     }
+    return primer_dato;
 }
 
 template<typename T, bool (* comp)(T, T)>
@@ -181,7 +238,9 @@ size_t heap<T, comp>::tamanio() {
 
 template<typename T, bool (* comp)(T, T)>
 heap<T, comp>::~heap() {
-
+    while (!vacio()){
+        baja();
+    }
 }
 
 #endif
