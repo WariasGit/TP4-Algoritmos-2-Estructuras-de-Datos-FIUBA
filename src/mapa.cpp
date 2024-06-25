404541db0e599675c107f79befdbce4970747fc5
Gise Zajic <106554970+gisela-zajic@users.noreply.github.com>
2024-06-25 16:00:33 -0300

    Feature/Algoritmo A* & Implementación de heurística (#4)
    Include algorithm en A*
    Defining manhattan distance on heuristica.hpp
    Updating CMakeLists.txt
    Adding new public methods on mapa.hpp with Pre and Post conditions
    Adding the implementation for all methods from mapa.hpp
    Adding a new public method for vertice.hpp.
    Adding a new public method for vertice.cpp.
    Adding the implementation for A* algorithm
    Updating missing changes from main repository
    Reverting last commit
    Adding missing comment

--- src/mapa.cpp
+++ src/mapa.cpp
@@ -1,5 +1,47 @@
 #include "mapa.hpp"
 
 bool mapa::es_vecino_valido(coordenada posicion) {
-    // TODO: Implementar.
+    auto fila = static_cast<size_t>(posicion.y());
+    auto columna = static_cast<size_t>(posicion.x());
+
+    if (fila < 1 || fila >= matriz_caminable.filas()) {
+        return false;
+    }
+
+    if (columna < 1 || columna >= matriz_caminable.columnas()) {
+        return false;
+    }
+
+    return matriz_caminable.elemento(fila, columna);
+}
+
+mapa::mapa(size_t filas, size_t columnas) {
+    matriz_caminable = matriz<bool>(filas, columnas, true);
+}
+
+void mapa::agregar_no_caminable(coordenada posicion) {
+    auto fila = static_cast<size_t>(posicion.y());
+    auto columna = static_cast<size_t>(posicion.x());
+    matriz_caminable.elemento(fila, columna) = false;
+}
+
+std::list<coordenada> mapa::obtener_vecinos_valido(coordenada posicion) {
+    std::list<coordenada> vecinos;
+
+    for (auto vecino : obtener_vecinos(posicion)) {
+        if (es_vecino_valido(vecino)) {
+            vecinos.push_back(vecino);
+        }
+    }
+
+    return vecinos;
+}
+
+std::list<coordenada> mapa::obtener_vecinos(coordenada posicion) {
+    std::list<coordenada> vecinos;
+    vecinos.emplace_back(posicion.x() - 1, posicion.y());
+    vecinos.emplace_back(posicion.x() + 1, posicion.y());
+    vecinos.emplace_back(posicion.x(), posicion.y() - 1);
+    vecinos.emplace_back(posicion.x(), posicion.y() + 1);
+    return vecinos;
 }
